3b74f4519191208ff381f9c3cc5542fd99370115
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-03-05 08:45:14 +0000

    Renamed projects to remove space in name

--- My Website/MyWebsite/Program.cs
+++ My Website/MyWebsite/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
-using My_Website;
+using MyWebsite;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
