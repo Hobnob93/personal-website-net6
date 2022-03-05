7193b34e7f04501490ccc54b724a8be8e9723848
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-03-05 13:14:58 +0000

    Added a component for making text coloured and bold within a text segment

---
+++ My Website/MyWebsite/Shared/Bolden.razor.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components;
+
+namespace MyWebsite.Shared;
+
+public partial class Bolden
+{
+    [Parameter] public string ColorClass { get; set; } = "mud-secondary-text";
+    [Parameter] public bool MakeBold { get; set; } = true;
+    [Parameter] public string Text { get; set; } = "<bold text not provided>";
+    
+    [Parameter] public RenderFragment ChildContent {get;set;}
+}
