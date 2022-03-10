dbdb889d94406deb0cdd216eea1462bce91c5760
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2022-03-10 10:00:41 +0000

    Slight adjustment to welcome text + bolden no longer defaults to bold

--- My Website/MyWebsite/Shared/Bolden.razor.cs
+++ My Website/MyWebsite/Shared/Bolden.razor.cs
@@ -5,7 +5,7 @@ namespace MyWebsite.Shared;
 public partial class Bolden
 {
     [Parameter] public string ColorClass { get; set; } = "mud-secondary-text";
-    [Parameter] public bool MakeBold { get; set; } = true;
+    [Parameter] public bool MakeBold { get; set; }
     [Parameter] public string Text { get; set; } = "<bold text not provided>";
     
     [Parameter] public RenderFragment? ChildContent { get; set; }
