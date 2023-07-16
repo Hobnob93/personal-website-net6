9238d737ab0dff2b75da114b947fb692740f8d5a
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2023-07-16 13:26:58 +0100

    FlexBox component

--- PersonalSite/Website.Components/Layout/FlexBox.razor.cs
+++ PersonalSite/Website.Components/Layout/FlexBox.razor.cs
@@ -3,13 +3,21 @@ using Website.Components.Builders;
 
 namespace Website.Components.Layout;
 
-public partial class Hero : BaseComponent
+public partial class FlexBox : BaseComponent
 {
     [Parameter, EditorRequired]
     public RenderFragment ChildContent { get; set; } = default!;
 
+    [Parameter]
+    public bool IsColumn { get; set; }
+
+    [Parameter]
+    public bool FullHeight { get; set; }
+
     public string Classes => new ClassBuilder()
-        .Add("hero")
+        .Add("d-flex")
+        .Add("flex-column", condition: IsColumn)
+        .Add("h-100pc", condition: FullHeight)
         .Add(Class!, condition: Class is not null)
         .Build();
 }
--- PersonalSite/Website.Components/Utility/Loading.razor.cs
+++ PersonalSite/Website.Components/Utility/Loading.razor.cs
@@ -1,6 +1,11 @@
-﻿namespace Website.Components.Utility;
+﻿using Website.Components.Builders;
+
+namespace Website.Components.Utility;
 
 public partial class Loading : BaseComponent
 {
-    
+    public string Classes => new ClassBuilder()
+        .Add("loading-content")
+        .Add(Class!, condition: Class is not null)
+        .Build();
 }
