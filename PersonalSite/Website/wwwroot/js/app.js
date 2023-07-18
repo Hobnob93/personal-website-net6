67f57f6f04966402c6916377380470d4d9b42b5b
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2023-07-18 22:43:50 +0100

    Fixed mouse gradient not being relative to window

--- PersonalSite/Website/wwwroot/js/app.js
+++ PersonalSite/Website/wwwroot/js/app.js
@@ -1,8 +1,7 @@
 let btn = document.querySelector('.mouse-cursor-gradient');
 btn.addEventListener('mousemove', e => {
-    let rect = e.target.getBoundingClientRect();
-    let x = e.clientX - rect.left;
-    let y = e.clientY - rect.top;
+    let x = e.clientX;
+    let y = e.clientY;
     btn.style.setProperty('--mouse-x', x + 'px');
     btn.style.setProperty('--mouse-y', y + 'px');
 });
