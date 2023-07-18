5fbdbbd4d737dde870104c955606fdbb7f9180fd
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2023-07-18 22:58:37 +0100

    Randomly sizing tiles on page load

---
+++ PersonalSite/Website/Pages/Index.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Website.Pages;
+
+public partial class Index
+{
+    [Inject]
+    private IJSRuntime JsRuntime { get; set; } = default!;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender)
+        {
+            await JsRuntime.InvokeVoidAsync("randomlySizeHeroColumns");
+        }
+    }
+}
--- PersonalSite/Website/wwwroot/js/app.js
+++ PersonalSite/Website/wwwroot/js/app.js
@@ -1,7 +1,22 @@
-let btn = document.querySelector('.mouse-cursor-gradient');
-btn.addEventListener('mousemove', e => {
+let div = document.querySelector('.mouse-cursor-gradient');
+div.addEventListener('mousemove', e => {
     let x = e.clientX;
     let y = e.clientY;
-    btn.style.setProperty('--mouse-x', x + 'px');
-    btn.style.setProperty('--mouse-y', y + 'px');
+    div.style.setProperty('--mouse-x', x + 'px');
+    div.style.setProperty('--mouse-y', y + 'px');
 });
+
+function randomlySizeHeroColumns() {
+    let heroColumns = document.querySelectorAll('.hero-column');
+    heroColumns.forEach(column => {
+        var topHeight = randomIntFromInterval(40, 60);
+        var bottomHeight = 100 - topHeight;
+
+        column.children[0].style.setProperty('--hero-tile-height', topHeight + 'vh');
+        column.children[1].style.setProperty('--hero-tile-height', bottomHeight + 'vh');
+    });
+}
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
