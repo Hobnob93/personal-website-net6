411ab7dba74c681375451fee72cd4e86cfb80985
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2023-07-20 23:09:43 +0100

    Removed flex boxes for same effect

--- PersonalSite/Website.Components/Layout/HeroTile.razor.cs
+++ PersonalSite/Website.Components/Layout/HeroTile.razor.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using Website.Components.Builders;
 
 namespace Website.Components.Layout;
@@ -6,11 +6,10 @@ namespace Website.Components.Layout;
 public partial class HeroTile : BaseComponent
 {
     [Parameter, EditorRequired]
-    public string Text { get; set; } = default!;
+    public string Text { get; set; } = string.Empty;
 
     public string Classes => new ClassBuilder()
         .Add("hero-tile")
-        .Add("flex-grow-1")
         .Add(Class!, condition: Class is not null)
         .Build();
 }
