b3fb05feca521ed269ca80070db6fb96a6da43d9
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2023-07-26 11:37:58 +0100

    Hero tile now shows image by name

--- PersonalSite/Website.Components/Layout/HeroTile.razor.cs
+++ PersonalSite/Website.Components/Layout/HeroTile.razor.cs
@@ -8,6 +8,12 @@ public partial class HeroTile : BaseComponent
     [Parameter, EditorRequired]
     public string Text { get; set; } = string.Empty;
 
+    [Parameter, EditorRequired]
+    public string ImageName { get; set; } = string.Empty;
+
+    private string ImagePath => $"/images/home/{ImageName}.png";
+    private string ImageAlt => $"{ImageName} hero image";
+
     public string Classes => new ClassBuilder()
         .Add("hero-tile")
         .Add(Class!, condition: Class is not null)
