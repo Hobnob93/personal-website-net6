675179c286f77055e3fac04396751e65a70d16a1
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2023-07-28 15:23:33 +0100

    Hero tile name fading in as part of animation

--- PersonalSite/Website.Components/Layout/HeroTile.razor.cs
+++ PersonalSite/Website.Components/Layout/HeroTile.razor.cs
@@ -6,13 +6,13 @@ namespace Website.Components.Layout;
 public partial class HeroTile : BaseComponent
 {
     [Parameter, EditorRequired]
-    public string Text { get; set; } = string.Empty;
+    public string Char { get; set; } = string.Empty;
 
     [Parameter, EditorRequired]
-    public string ImageName { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
 
-    private string ImagePath => $"/images/home/{ImageName}.png";
-    private string ImageAlt => $"{ImageName} hero image";
+    private string ImagePath => $"/images/home/{Name}.png";
+    private string ImageAlt => $"{Name} hero image";
 
     public string Classes => new ClassBuilder()
         .Add("hero-tile")
