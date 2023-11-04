24a7c3664ed14db6f8f67bbad02e883a06a89ca4
Kyle Hobdey <kyle.hobdey@hotmail.co.uk>
2023-11-04 16:50:42 +0000

    Added hero component designed for topic landing pages

--- PersonalSite/Website.Components/Layout/Hero.razor.cs
+++ PersonalSite/Website.Components/Layout/Hero.razor.cs
@@ -1,6 +1,25 @@
+using Microsoft.AspNetCore.Components;
+using System.Runtime.CompilerServices;
+using Website.Components.Builders;
+
 namespace Website.Components.Layout;
 
-public partial class Hero
+public partial class Hero : BaseSiteComponent
 {
-    
+    [Parameter, EditorRequired]
+    public string Title { get; set; } = string.Empty;
+
+    [Parameter]
+    public string ImageType { get; set; } = "png";
+
+    private string ImagePath => $"/images/home/{Title}Hero.{ImageType}";
+
+    public string Classes => new ClassBuilder()
+        .Add("hero")
+        .Add(Class!, condition: Class is not null)
+        .Build();
+
+    public string Styles => new CssBuilder(Style ?? string.Empty)
+        .Add("background", $"url({ImagePath})")
+        .Build();
 }
